b0751351a732f18df57839c237abf1eedb2831ea
Frank Adelardi <fmadelardi@gmail.com>
2017-03-25 21:27:35 +0100

    Adding linkable title

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,7 @@
 
   @foreach($posts as $post)
     <div>
+      <div><a href="/posts/{{ $post->id }}" >{{$post->title}}</a></div>
       <div>{{$post->created_at}}</div>
       <div>{{$post->body}}</div>
     </div>
