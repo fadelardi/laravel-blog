1aed119ec1ee068786e46ab303f344e813ec2a98
Frank Adelardi <fmadelardi@gmail.com>
2017-03-25 20:46:04 +0100

    Displaying comments

--- app/Posts.php
+++ app/Posts.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     protected $fillable = ['title', 'body'];
+
+    public function comments()
+    {
+      return $this->hasMany(Comments::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,5 +4,12 @@
     <div>
       <div>{{$post->created_at}}</div>
       <div>{{$post->body}}</div>
+
+      <hr>
+
+      @foreach($post->comments as $comment)
+        <div>{{ $comment->body }}</div>
+      @endforeach
+
     </div>
 @endsection
