daed54132330587270cd83164c81ea12b4ed2751
Frank Adelardi <fmadelardi@gmail.com>
2017-03-28 22:49:43 +0200

    Styling create post form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,16 +2,23 @@
 
 @section('content')
 @include('errors')
-<form method="POST" action="/posts">
-{{ csrf_field() }}
-<div>
-  <label>Title</title>
-  <input type="text" name="title"  />
-<div>
-<div>
-  <label>Body</label>
-  <textarea name="body" required></textarea>
+<div class="panel panel-default">
+  <div class="panel-heading">
+      Create Post
+  </div>
+  <div class="panel-body">
+    <form method="POST" action="/posts">
+      {{ csrf_field() }}
+      <div class="form-group">
+        <label>Title</label>
+        <input type="text" name="title" class="form-control" required  />
+      </div>
+      <div class="form-group">
+        <label>Body</label>
+        <textarea name="body" class="form-control" required></textarea>
+      </div>
+      <input type="submit" value="Publish" class="btn btn-primary" />
+    </form>
+  </div>
 </div>
-<input type="submit" value="Publish" />
-</form>
 @endsection
