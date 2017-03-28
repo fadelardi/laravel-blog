c202fbc8c2316d3391015cbc3f58e381a222ce91
Frank Adelardi <fmadelardi@gmail.com>
2017-03-28 22:44:39 +0200

    Styling login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
   @include('errors')
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      Sign in
+    </div>
+    <div class="panel-body">
+      <form method="POST" action="/login">
+      {{ csrf_field() }}
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control" required  />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" required  />
+      </div>
 
-  <form method="POST" action="/login">
-  {{ csrf_field() }}
-  <div>
-    <label>Email</label>
-    <input type="email" name="email" required  />
+      <input type="submit" value="Log In" class="btn btn-primary" />
+      </form>
+    </div>
   </div>
-  <div>
-    <label>Password</label>
-    <input type="password" name="password" required  />
-  </div>
-
-  <input type="submit" value="Log In" />
-  </form>
 @endsection
