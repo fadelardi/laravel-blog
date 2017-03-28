3d6edb104fa7b771ca43bc6336b711a1b56fc67b
Frank Adelardi <fmadelardi@gmail.com>
2017-03-28 22:47:12 +0200

    Styling registration form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,22 +2,28 @@
 
 @section('content')
   @include('errors')
+<div class="panel panel-default">
+  <div class="panel-heading">
+    Register
+  </div>
+  <div class="panel-body">
+    <form method="POST" action="/register">
+    {{ csrf_field() }}
+    <div class="form-group">
+      <label>Name</label>
+      <input type="text" name="name" class="form-control" required  />
+    </div>
+    <div class="form-group">
+      <label>Email</label>
+      <input type="email" name="email" class="form-control" required  />
+    </div>
+    <div class="form-group">
+      <label>Password</label>
+      <input type="password" name="password" class="form-control" required  />
+    </div>
 
-  <form method="POST" action="/register">
-  {{ csrf_field() }}
-  <div>
-    <label>Name</label>
-    <input type="text" name="name" required  />
+    <input type="submit" value="Register" class="btn btn-primary" />
+    </form>
   </div>
-  <div>
-    <label>Email</label>
-    <input type="email" name="email" required  />
-  </div>
-  <div>
-    <label>Password</label>
-    <input type="password" name="password" required  />
-  </div>
-
-  <input type="submit" value="Register" />
-  </form>
+</div>
 @endsection
