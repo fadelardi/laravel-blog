25726399c3ffbdf925df71b8554839df03582fba
Frank Adelardi <fmadelardi@gmail.com>
2017-03-28 22:31:30 +0200

    Styling navigation

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,13 +1,17 @@
- <nav>
-  <a href="/">Home</a> |
-  <a href="/login">Login</a> |
-  <a href="/register">Register</a> |
-  @if (Auth::check())
-    <a href="/posts/create">Create Post</a> |
-    <a href="/logout">Logout</a>
-    <div style="margin-left:auto">{{ Auth::user()->name }}</div>
-  @endif
+ <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+      <li><a href="/">Home</a></li>
+      <li><a href="/login">Login</a></li>
+      <li><a href="/register">Register</a></li>
+      @if (Auth::check())
+        <li><a href="/posts/create">Create Post</a></li>
+        <li><a href="/logout">Logout</a></li>
+      @endif
+    </ul>
 
+    @if (Auth::check())
+      <div style="margin-left:auto">{{ Auth::user()->name }}</div>
+    @endif
+  </div>
 </nav>
-
-<hr>
